396b450882ec6ef959c85827014ed9e93ff814b7
diyaS-15 <122123763+diyaS-15@users.noreply.github.com>
2026-03-03 19:11:06 -0700

    Update train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 from sklearn.ensemble import HistGradientBoostingClassifier
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import (accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix)
 
 from clean_missing import clean_missing_values
 clean_missing_values("synthetic_data.csv", "cleaned_data.csv")
@@ -18,3 +19,9 @@ model = HistGradientBoostingClassifier()
 model.fit(X_scaled, y)
 
 print("trained")
+
+y_pred = model.predict(X)
+y_proba = model.predict_proba(X)[:, 1]
+
+print(accuracy_score(y, y_pred), precision_score(y, y_pred), recall_score(y, y_pred),f1_score(y, y_pred), roc_auc_score(y, y_proba))
+
